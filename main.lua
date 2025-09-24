5d5a6168a99b5eb2332909a900b06351c1a8cac1
GeorgeTheRat <earth.guy422@gmail.com>
2025-09-24 07:49:39 -0400

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,3 +1,40 @@
+local XmainMenuHook = Game.main_menu
+function Game:main_menu(ctx)
+    local r = XmainMenuHook(self,ctx)
+    if self.title_top then
+        local tg = self.title_top
+        local card = Card(tg.T.x,tg.T.y,G.CARD_W,G.CARD_H,nil,G.P_CENTERS['c_solo_lady'])
+        card.bypass_discovery_center = true
+        card.T.w = card.T.w * 1.35
+        card.T.h = card.T.h * 1.35
+        
+        G.title_top.T.w = G.title_top.T.w + 1.35
+        G.title_top.T.x = G.title_top.T.x - 0.7
+        card:set_sprites(card.config.center)
+        card.no_ui = true
+        card.states.visible = false
+        self.title_top:emplace(card)
+        self.title_top:align_cards()
+        G.E_MANAGER:add_event(
+            Event{
+                delay = 0.5,
+                func = function ()
+                    if ctx == "splash" then
+                        card.states.visible = true
+                        card:start_materialize({ G.C.WHITE, G.C.WHITE }, true, 0.5)
+                    else
+                        card.states.visible = true
+                        card:start_materialize({ G.C.WHITE, G.C.WHITE }, nil, 0.2)
+                    end
+                    return true
+                end
+            }
+        )
+    end
+
+    return r
+end
+
 SMODS.Atlas({
     key = "modicon", 
     path = "ModIcon.png", 
