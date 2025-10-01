74ff7b9dfd05ecebe0214439974c0a1ee2e4a112
GeorgeTheRat <earth.guy422@gmail.com>
2025-09-30 22:52:17 -0400

    Add files via upload

--- main.lua
+++ main.lua
@@ -4,7 +4,7 @@ SMODS.Atlas({
     px = 34,
     py = 34,
     atlas_table = "ASSET_ATLAS"
-}):register()
+})
 
 SMODS.Atlas({
     key = "CustomJokers", 
@@ -12,7 +12,7 @@ SMODS.Atlas({
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
-}):register()
+})
 
 SMODS.Atlas({
     key = "CustomConsumables", 
@@ -20,7 +20,7 @@ SMODS.Atlas({
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
-}):register()
+})
 
 SMODS.Atlas({
     key = "CustomBoosters", 
@@ -28,7 +28,7 @@ SMODS.Atlas({
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
-}):register()
+})
 
 SMODS.Atlas({
     key = "CustomEnhancements", 
@@ -36,47 +36,67 @@ SMODS.Atlas({
     px = 71,
     py = 95, 
     atlas_table = "ASSET_ATLAS"
-}):register()
+})
 
 local NFS = require("nativefs")
 to_big = to_big or function(a) return a end
 lenient_bignum = lenient_bignum or function(a) return a end
 
+local jokerIndexList = {45,1,2,3,4,5,7,10,11,12,13,14,15,17,21,22,24,25,26,27,28,29,30,31,33,34,38,39,40,41,43,44,18,20,19,36,32,23,6,16,37,35,8,42,9}
+
 local function load_jokers_folder()
     local mod_path = SMODS.current_mod.path
     local jokers_path = mod_path .. "/jokers"
     local files = NFS.getDirectoryItemsInfo(jokers_path)
-    for i = 1, #files do
-        local file_name = files[i].name
+    for i = 1, #jokerIndexList do
+        local file_name = files[jokerIndexList[i]].name
         if file_name:sub(-4) == ".lua" then
             assert(SMODS.load_file("jokers/" .. file_name))()
         end
     end
 end
 
+
+local consumableIndexList = {36,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25,26,27,28,29,30,31,32,33,34,35}
+
 local function load_consumables_folder()
     local mod_path = SMODS.current_mod.path
     local consumables_path = mod_path .. "/consumables"
     local files = NFS.getDirectoryItemsInfo(consumables_path)
+    local set_file_number = #files + 1
     for i = 1, #files do
-        local file_name = files[i].name
+        if files[i].name == "sets.lua" then
+            assert(SMODS.load_file("consumables/sets.lua"))()
+            set_file_number = i
+        end
+    end    
+    for i = 1, #consumableIndexList do
+        local j = consumableIndexList[i]
+        if j >= set_file_number then 
+            j = j + 1
+        end
+        local file_name = files[j].name
         if file_name:sub(-4) == ".lua" then
             assert(SMODS.load_file("consumables/" .. file_name))()
         end
     end
 end
 
+
+local enhancementIndexList = {8,1,2,3,4,5,6,7}
+
 local function load_enhancements_folder()
     local mod_path = SMODS.current_mod.path
     local enhancements_path = mod_path .. "/enhancements"
     local files = NFS.getDirectoryItemsInfo(enhancements_path)
-    for i = 1, #files do
-        local file_name = files[i].name
+    for i = 1, #enhancementIndexList do
+        local file_name = files[enhancementIndexList[i]].name
         if file_name:sub(-4) == ".lua" then
             assert(SMODS.load_file("enhancements/" .. file_name))()
         end
     end
 end
+
 
 local function load_boosters_file()
     local mod_path = SMODS.current_mod.path
@@ -107,7 +127,9 @@ SMODS.ObjectType({
     cards = {
         ["j_solo_bingocard"] = true,
         ["j_solo_keychain"] = true,
-        ["j_solo_motherboard"] = true
+        ["j_solo_koi"] = true,
+        ["j_solo_motherboard"] = true,
+        ["j_solo_onigiri"] = true
     },
 })
 
