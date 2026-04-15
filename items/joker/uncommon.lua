e5b88a47aefb0b061627dea88557c01b95c8b64e
kierkat10 <kiernan.simpson@gmail.com>
2026-04-14 21:39:36 -0700

    motherboard fixes

--- items/joker/uncommon.lua
+++ items/joker/uncommon.lua
@@ -682,6 +682,7 @@ SMODS.Joker {
             chips = 0
         }
     },
+    pixel_size = { h = 71 },
     pos = { x = 1, y = 2 },
     cost = 5,
     rarity = 2,
@@ -704,9 +705,25 @@ SMODS.Joker {
         end
         if context.pseudorandom_result then
             if context.result then
-                card.ability.extra.chips = math.max(0, (card.ability.extra.chips) - card.ability.extra.chips_mod)
+                SMODS.scale_card(card, {
+                    ref_table = card.ability.extra,
+                    ref_value = "chips",
+                    scalar_value = "chips_mod",
+                    message_colour = G.C.CHIPS,
+                    operation = function(ref_table, ref_value, initial, change)
+                        ref_table[ref_value] = ref_table[ref_value] - change
+                        if ref_table[ref_value] <= 0 then
+                            ref_table[ref_value] = 0
+                        end
+                    end
+                })
             else
-                card.ability.extra.chips = (card.ability.extra.chips) + card.ability.extra.chips_mod_2
+                SMODS.scale_card(card, {
+                    ref_table = card.ability.extra,
+                    ref_value = "chips",
+                    scalar_value = "chips_mod_2",
+                    message_colour = G.C.CHIPS
+                })
             end
         end
     end
