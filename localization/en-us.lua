4a05d18c333b200bb69afd55c02691e3a55806a8
kierkat10 <kiernan.simpson@gmail.com>
2026-06-01 12:29:27 -0700

    loc fix

--- localization/en-us.lua
+++ localization/en-us.lua
@@ -677,8 +677,8 @@ return {
 			m_chm_doodle = {
 				name = "Doodle",
 				text = {
-					"{C:green}#1# in #2#{} chance to give {C:mult}+#3#{} Mult",
-					"{C:green}#4# in #5#{} chance to give {C:chips}+#6#{} chip#<s>6#",
+					"{C:green}#1# in #2#{} chance to give {C:mult}+#3#{} chip#<s>3#",
+					"{C:green}#4# in #5#{} chance to give {C:mult}+#6#{} Mult",
 					"{C:green}#7# in #8#{} chance to give {C:white,X:mult}X#9#{} Mult",
 					"{C:green}#10# in #11#{} chance to earn {C:money}$#12#",
 				}
